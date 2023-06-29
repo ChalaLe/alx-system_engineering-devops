b6c89cf32c62665a2d160a3725ac80f7b67c5ad4
ChalaLe <clelisa28@gmail.com>
2023-06-29 00:22:04 +0000

    task0 update

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,7 @@
-file { '/tmp/school':
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',
+file {'/tmp/school':
+    mode       => '0744',
+    owner      => www-data,
+    group      => www-data,
+    content    => 'I love Puppet'
 }
+

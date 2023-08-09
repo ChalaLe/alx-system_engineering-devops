bccea21bdcdb2b367b203f21e167764dec7c46d0
ChalaLe <clelisa28@gmail.com>
2023-08-09 18:05:39 +0000

    wsd

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,8 @@
+# 0-strace_is_your_friend.pp
+
+# This Puppet manifest automates the fix for Apache 500 Internal Server Error
+exec { 'fix_apache_error':
+  path        => ['/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/'],
+  command     => 'mv /var/www/html/wp-includes/class-wp-locale.php /var/www/html/wp-includes/class-wp-locale.phpp',
+  refreshonly => true,
+}

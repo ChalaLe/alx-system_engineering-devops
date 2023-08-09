c2a0a9c2eeb2d0ed06803d72dca5500eb0c1fd6c
ChalaLe <clelisa28@gmail.com>
2023-08-09 00:44:06 +0000

    first commit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Function to query subscribers on a given Reddit subreddit."""
+
+import requests  # Import the requests library to make HTTP requests
+
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
+    
+    # Construct the URL for the subreddit's about page
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    
+    # Define the User-Agent header to provide information about your application
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/ChalaLe)"
+    }
+    
+    # Send a GET request to the API with the specified URL and headers
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    
+    # Check if the response status code is 404 (Not Found)
+    if response.status_code == 404:
+        return 0  # Return 0 if the subreddit is not found
+    
+    # Parse the JSON response and extract the "data" section
+    results = response.json().get("data")
+    
+    # Return the number of subscribers from the "data" section
+    return results.get("subscribers")
+
+# Example usage
+subreddit_name = "programming"
+subscribers_count = number_of_subscribers(subreddit_name)
+if subscribers_count > 0:
+    print(f"The subreddit r/{subreddit_name} has {subscribers_count} subscribers.")
+else:
+    print(f"The subreddit r/{subreddit_name} is not valid or couldn't be found.")

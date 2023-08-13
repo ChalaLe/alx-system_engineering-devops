73ae6bf4f1b48b32cc0dd1110a3541fadf8836df
ChalaLe <clelisa28@gmail.com>
2023-08-13 09:33:56 +0000

    update

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,4 @@
 # 0-strace_is_your_friend.pp
-
 # This Puppet manifest automates the fix for Apache 500 Internal Server Error
 exec { 'fix_apache_error':
   path        => ['/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/'],
